2a16c720f833ac3ea1d05a00661c8e681b0935db
Fredrick Myers <fmyers@legolas.gateway.2wire.net>
2014-02-13 15:48:06 -0500

    Initial Commit

---
+++ Pirate Game/FMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FMAppDelegate.h
+//  Pirate Game
+//
+//  Created by Fredrick Myers on 2/13/14.
+//  Copyright (c) 2014 Fredrick Myers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Pirate Game/FMViewController.h
@@ -0,0 +1,13 @@
+//
+//  FMViewController.h
+//  Pirate Game
+//
+//  Created by Fredrick Myers on 2/13/14.
+//  Copyright (c) 2014 Fredrick Myers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FMViewController : UIViewController
+
+@end
